f76af92ef967821d17803a7ebe0e3251673fe188
Nandini <nandinimehrotra14@gmail.com>
2023-01-12 18:09:38 -0500

    css for card

--- src/Card.js
+++ src/Card.js
@@ -10,19 +10,19 @@ function Info(props) {
     <div className='main-div'>
       <main>
         <div classname="info">
-      <Card sx={{width: '70%', marginLeft: '15%', marginTop: '5%'}}>
+      <Card sx={{width: '30%', marginLeft: '35%', marginTop: '5%'}}>
       <CardContent>
         <Typography style={{textAlign: 'center'}}> <b>Today's Forecast </b><br></br> </Typography>
         <Typography style={{textAlign: 'center'}} className="image-wrapper">
         <img src={"http://openweathermap.org/img/wn/" + props.res.weather?.[0].icon + "@2x.png"} class="icon" />
         </Typography>
         <Typography gutterBottom component="div" style={{textAlign: 'center'}}>
-          {props.condition}
+          <b>{props.condition}</b>
         </Typography>
         <Typography variant="body1" color="text.primary" style={{textAlign: 'center'}}>
-          {props.res.weather?.[0].description} <br></br>
-          Temperature: {Temp} <br></br>
-          Feels Like: {props.feels_like} <br></br>
+          <b>{props.res.weather?.[0].description} </b><br></br>
+          <b>Temperature: </b>{Temp} <br></br>
+          <b>Feels Like: </b>{props.feels_like} <br></br>
         </Typography>
         </CardContent>
     </Card>
