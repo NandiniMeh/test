fa5b3100e5439192e37f3652aa7ab7e942cbfdae
Nandini <nandinimehrotra14@gmail.com>
2023-01-13 03:11:37 -0500

    adding date and time

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,14 @@ import { CardContent } from "@mui/material";
 import { Typography } from "@mui/material";
 
 function Info(props) {
-    const Temp = props.T
+    const Temp = props.T;
+    var currentdate = new Date(); 
+    var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
   return (
     <div className='main-card'>
       <main>
@@ -12,6 +19,9 @@ function Info(props) {
       <div style={{width: '28%', marginLeft: '35%', marginTop: '5%'}} className="card-wrapper">
       <CardContent className="card">
         <Typography style={{textAlign: 'center'}}> <b>Today's Forecast </b><br></br> </Typography>
+        <Typography style={{textAlign: 'center'}}>
+          {datetime}
+        </Typography>
         <Typography style={{textAlign: 'center'}} className="image-wrapper">
         <img src={"http://openweathermap.org/img/wn/" + props.res.weather?.[0].icon + "@2x.png"} class="icon" />
         </Typography>
